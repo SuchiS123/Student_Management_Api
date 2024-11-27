a4f6ea19f14dff5731c7a75093f420292092d58b
Your Name <your.email@example.com>
2024-11-27 14:36:46 +0530

    defect-fixed-3245: was returning null values as response in postMan

--- src/main/java/com/student/students/studentController/StudentController.java
+++ src/main/java/com/student/students/studentController/StudentController.java
@@ -49,6 +49,10 @@ public class StudentController {
     {
         StudentDto studentDto=studentService.updateStudent(studentId,stdto);
         studentDto.setDate(new Date());
+        System.out.println("Student");
+        System.out.println("Students updated");
+        System.out.println("Student");
+        System.out.println("Students updated");
 
         return new ResponseEntity<>(studentDto,HttpStatus.OK);
 
